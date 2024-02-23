6c902f7cbc6acfe9a3dfcded267bb5b85b5d6d2a
Loc Tran <mrlocspoc@gmail.com>
2024-02-24 09:22:36 +1030

    21. Layout Styling complete

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,10 +5,10 @@ import './globals.css';
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body>
+			<body className="flex flex-col px-4 py-2 min-h-screen">
 				<header>
 					<nav>
-						<ul>
+						<ul className="flex gap-2">
 							<li>
 								<Link href="/">Home</Link>
 							</li>
@@ -23,8 +23,8 @@ export default function RootLayout({ children }) {
 						</ul>
 					</nav>
 				</header>
-				<main>{children}</main>
-				<footer>
+				<main className="grow py-3">{children}</main>
+				<footer className="border-t py-3 text-center text-xs">
 					Game data and images courtesy of{' '}
 					<a href="https://rawg.io" target="_blank">
 						RAWG
