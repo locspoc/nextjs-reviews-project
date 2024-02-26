68c9534804a353920579dcae0a2374cbbc57353b
Loc Tran <mrlocspoc@gmail.com>
2024-02-26 23:06:08 +1030

    26. Image Cards complete

--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -6,7 +6,7 @@ export default function StardewValleyPage() {
 			<Heading>Stardew Valley</Heading>
 			<img
 				src="/images/stardew-valley.jpg"
-				alt="Hollow Knight"
+				alt="Stardew Valley"
 				width="640"
 				height="360"
 				className="mb-2 rounded"
