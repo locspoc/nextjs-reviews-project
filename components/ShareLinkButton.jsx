e540896d4c1126632ae3a7e6bd4fe8148749c3e7
Loc Tran <mrlocspoc@gmail.com>
2024-03-16 11:46:21 +1030

    47. Component State complete

--- components/ShareLinkButton.jsx
+++ components/ShareLinkButton.jsx
@@ -1,15 +1,20 @@
 'use client';
 
+import { useState } from 'react';
+
 export default function ShareLinkButton() {
+	const [clicked, setClicked] = useState();
 	const handleClick = () => {
-		console.log('clicked!');
+		navigator.clipboard.writeText(window.location.href);
+		setClicked(true);
+		setTimeout(() => setClicked(false), 1500);
 	};
 	return (
 		<button
 			onClick={handleClick}
 			class="border px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
 		>
-			Share Link
+			{clicked ? 'Link copied' : 'Share Link'}
 		</button>
 	);
 }
