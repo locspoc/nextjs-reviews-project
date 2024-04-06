8f631de7e56c05f2682255af81feea734543fe55
Loc Tran <mrlocspoc@gmail.com>
2024-04-06 15:38:17 +1030

    Invalid populate parameter. Expected a string, an array of strings, a populate object

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,6 +1,19 @@
 import { writeFileSync } from 'node:fs';
+import qs from 'qs';
 
-const url = 'http://localhost:1337/api/reviews' + '?populate=*';
+const url =
+	'http://localhost:1337/api/reviews' +
+	'?populate=*' +
+	'?' +
+	qs.stringify(
+		{
+			fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+			populate: { image: { fields: ['url'] } },
+			// pagination: { pageSize: 6 },
+		},
+		{ encodeValueOnly: true }
+	);
+console.log('url: ', url);
 const response = await fetch(url);
 const body = await response.json();
 const formatted = JSON.stringify(body, null, 2);
