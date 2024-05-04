e5d688db584fc5d8787fc9ad74a6d20c8afa29a6
Loc Tran <mrlocspoc@gmail.com>
2024-05-04 20:46:05 +0930

    72. Exercise: Home Images complete

--- app/page.jsx
+++ app/page.jsx
@@ -1,32 +1,40 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { getFeaturedReview } from '@/lib/reviews';
+import { getReviews } from '@/lib/reviews';
 import Heading from '@/components/Heading';
 
 export default async function HomePage() {
-	const review = await getFeaturedReview();
+	const reviews = await getReviews(3);
 	return (
 		<>
 			<Heading>Indie Gamer</Heading>
 			<p className="pb-3">Only the best indie games, reviewed for you.</p>
-			<div className="bg-white border rounded shadow w-80 hover:shadow-xl sm:w-full">
-				<Link
-					href={`/reviews/${review.slug}`}
-					className="flex flex-col sm:flex-row"
-				>
-					<Image
-						src={review.image}
-						alt={review.title}
-						width="320"
-						height="180"
-						className="rounded-t sm:rounded-l sm:rounded-r-none"
-					/>
-					<h2 className="font-orbitron font-semibold py-1 text-center sm:px-2">
-						{review.title}
-					</h2>
-				</Link>
-			</div>
+			<ul className="flex flex-col gap-3">
+				{reviews.map((review, index) => (
+					<li
+						key={review.slug}
+						className="bg-white border rounded shadow w-80 hover:shadow-xl sm:w-full"
+					>
+						<Link
+							href={`/reviews/${review.slug}`}
+							className="flex flex-col sm:flex-row"
+						>
+							<Image
+								src={review.image}
+								alt={review.title}
+								priority={index === 0}
+								width="320"
+								height="180"
+								className="rounded-t sm:rounded-l sm:rounded-r-none"
+							/>
+							<h2 className="font-orbitron font-semibold py-1 text-center sm:px-2">
+								{review.title}
+							</h2>
+						</Link>
+					</li>
+				))}
+			</ul>
 		</>
 	);
 }
