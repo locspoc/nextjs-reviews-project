1e4b81a3f88d48596a989c4ee674376385b466fe
Loc Tran <mrlocspoc@gmail.com>
2024-12-02 22:38:49 +1030

    116. Revalidate Path complete

--- scripts/test-db.mjs
+++ scripts/test-db.mjs
@@ -4,16 +4,16 @@ const db = new PrismaClient({
     log: [{ emit: 'stdout', level: 'query'}],
 });
 
-// const comment = await db.comment.create({
-//     data: {
-//         slug: 'stardew-valley',
-//         user: 'Bob',
-//         message: 'Test message 3',
-//     }
-// });
-// console.log('created: ', comment);
-
-const comments = await db.comment.findMany({
-    where: { slug: 'fall-guys' },
+const comment = await db.comment.create({
+    data: {
+        slug: 'fall-guys',
+        user: 'Dan',
+        message: 'A new message',
+    }
 });
-console.log('comments: ', comments);
+console.log('created: ', comment);
+
+// const comments = await db.comment.findMany({
+//     where: { slug: 'fall-guys' },
+// });
+// console.log('comments: ', comments);
